aa79b20d90407a38525a2f7154b33010df5ac35e
Murtaza Hussain <MHussain@bluechipww.com>
2017-02-07 20:00:10 -0600

    making API call retrieving house members, data dump to home page, guzzle installed

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,19 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use GuzzleHttp\Client;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    public function getCongressApiData()
+    {
+        $client = new Client();
+        $res = $client->request('get', 'https://api.propublica.org/congress/v1/114/house/members.json', [
+            'headers' => [
+                'X-Api-key' => 'PPo8NOUWRG9i9WcBKJVIVacNERznlT50adGL56wN'
+            ]
+        ]);
+        dd([json_decode($res->getBody())]);
+    }
 }
